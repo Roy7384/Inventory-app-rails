9974677ed4a8bc9dc9a384938d656f33450a90c0
Roy Liu <roylu7384@gmail.com>
2022-05-14 16:26:33 +0000

    Setup inventory creation test

---
+++ spec/features/inventory_creation_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "Inventory creation", type: :feature do
+  before do
+    visit new_inventory_path
+  end
+
+  it "saves the inventory properly and displays it to the user" do
+    fill_in "Product", with: "Water bottle"
+    click_link_or_button "Create Inventory"
+
+    expect(page).to have_content("Inventory created")
+    expect(page).to have_content("Water bottle")
+  end
+end

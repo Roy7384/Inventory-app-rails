5c30493f9a6f46181dcac853d01f7d4598879e8b
Roy Liu <roylu7384@gmail.com>
2022-05-14 20:55:08 +0000

    Update inventory quantity validation to be non-negative

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,6 +1,6 @@
 class Inventory < ApplicationRecord
 
   validates :product, presence: true
-  validates :quantity, numericality: { only_integer: true }
+  validates :quantity, numericality: { only_integer: true,  :greater_than_or_equal_to => 0 }
   
 end

fa58fbed0b6ac4dd621049bc4e8c204abe3fa47c
Roy Liu <roylu7384@gmail.com>
2022-05-14 21:46:29 +0000

    Add uniqueness constraint to product name

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,6 +1,6 @@
 class Inventory < ApplicationRecord
 
-  validates :product, presence: true
+  validates :product, presence: true, uniqueness: { case_sensitive: false, message: 'already exist' }
   validates :quantity, numericality: { only_integer: true,  :greater_than_or_equal_to => 0 }
   
 end

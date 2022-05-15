7979849810038c352455e528bfd35f9faeb56ed2
Roy Liu <roylu7384@gmail.com>
2022-05-15 01:10:03 +0000

    Implement delete_comments creation

--- app/controllers/delete_comments_controller.rb
+++ app/controllers/delete_comments_controller.rb
@@ -1,2 +1,11 @@
 class DeleteCommentsController < ApplicationController
+  protect_from_forgery with: :null_session
+  
+  def create 
+    @inventory = Inventory.find(params[:inventory_id])
+    @delete_comment = @inventory.delete_comments.create(params.require(:delete_comment).permit(:body))
+    @inventory.destroy
+    redirect_to inventories_path
+  end
+  
 end
